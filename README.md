cdad1a41b617f5d5c960a85ea56fab48aac307be
alessandromasone <masonealessandro04@gmail.com>
2022-09-17 22:05:04 +0200

    Update descrizioni
    Aggiunta dei commenti nel file principale e aggiunta di un file README

---
+++ README.md
@@ -0,0 +1,7 @@
+Essendo un informatico, ho deciso di creare questo mini applicativo, perchè il tecnico del mio laboratorio aveva cambiato lo sfondo del mio desktop con quello default di Windows!! Questo mi ha fatto arrabbiare perchè avevo disegnato uno sfondo molto stile "Vincent van Gogh". Il funzionamento è molto semplice basta avviarlo e si nasconderà all'interno dei processi del sistema. C'è la possibilità di usare un url personale per le immagini e di modificare ogni quanto cambiare l'immagine.
+
+I valori di default sono:
+<br>
+URL: https://picsum.photos/3840/2160
+<br>
+TEMPO: 5 minuti
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+#librerie di base
 try:
     import pywintypes
     import win32gui
     import win32con
-    hide = win32gui.GetForegroundWindow()
-    win32gui.ShowWindow(hide , win32con.SW_HIDE)
     import ctypes
     import pickle
     import argparse
@@ -19,6 +18,7 @@ try:
 except Exception as e:
     print(e.args)
     sys.exit()
+#funzione per la gestione dei parametri se passati da input
 def get_argv():
     try:
         parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -31,11 +31,14 @@ def get_argv():
     except Exception as e:
         print(e.args)
         sys.exit()
+#download dell'immagine da impostare come background
 def get_file(url: str, folder: str, name: str, ext):
     try:
+        #controllo esistenza cartella
         if (folder != ''):
             if not os.path.exists(folder):
                 os.makedirs(folder)
+        #richiesta e download dell'immagine
         with urllib.request.urlopen(url) as response:
             parsed_url_path = urllib.parse.urlparse(response.url).path
             filename = os.path.basename(parsed_url_path)
@@ -47,8 +50,10 @@ def get_file(url: str, folder: str, name: str, ext):
     except Exception as e:
         print(e.args)
         sys.exit()
+#controllo dei parametri di funzionalità
 def check_argv(argv):
     try:
+        #intervallo di tempo
         if (int(argv.time) < 1 or int(argv.time) > 2592000):
             print("Valore tempo non valido")
             sys.exit()
@@ -56,6 +61,7 @@ def check_argv(argv):
         print(e.args)
         sys.exit()
     try:
+        #validità url
         if (argv.url != None):
             result = urlparse(argv.url)
             response = requests.get(argv.url)
@@ -65,25 +71,32 @@ def check_argv(argv):
     except Exception as e:
         print(e.args)
         sys.exit()
+#controllo presenza connessione ad internet
 def connected_to_internet(url='http://www.google.com/', timeout=5):
     try:
         _ = requests.head(url, timeout=timeout)
         return True
     except requests.ConnectionError:
         sys.exit()
+#corpo principale del programma
 def main():
     try:
+        #nascondi schermata e controllo internet
+        hide = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(hide , win32con.SW_HIDE)
         connected_to_internet()
     except Exception as e:
         print(e.args)
         sys.exit()
     try:
+        #analisi parametri
         argv = get_argv()
         check_argv(argv)
     except Exception as e:
         print(e.args)
         sys.exit()
     try:
+        #associazione valori
         wait = int(argv.time)
         if (argv.url == None):
             url = 'https://picsum.photos/3840/2160'
@@ -91,18 +104,22 @@ def main():
     except Exception as e:
         print(e.args)
         sys.exit()
+    #ciclo ogni X tempo
     for i in count(0):
         try:
+            #cambio background
             SPI_SETDESKWALLPAPER = 20
             name_img = get_file(url, path_temp, 'bg', None)
             ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_temp + name_img, 3)
+            #pulizia file vecchi
             if os.path.exists(path_temp + name_img):
                 os.remove(path_temp + name_img)
-            time.sleep(wait * 60)
+            time.sleep(wait * 60) #attesa
         except Exception as e:
             print(e.args)
             shutil.rmtree(path_temp)
             sys.exit()
+#scelta inizio programma
 if __name__ == "__main__":
     try:
         main()
